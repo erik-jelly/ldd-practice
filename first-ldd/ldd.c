8e4f865bd8530793e98f9eef9a33c91aded1d885
erik-jelly <32176939+erik-jelly@users.noreply.github.com>
2026-08-12 23:23:50 -0400

    Solidified read/write ops and fixed remaining bugs
    Implemented read and write operations for proc file with no bugs. Added a parent folder in proc filesystem for the module. Added comments to explain code.

--- first-ldd/ldd.c
+++ first-ldd/ldd.c
@@ -2,33 +2,68 @@
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 
-MODULE_LICENSE("GLP");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Erik Jelezarov");
 MODULE_DESCRIPTION("Basic interactable linux device driver that reacts to read and write ops");
 
+static struct proc_dir_entry *proc_folder;
+static struct proc_dir_entry *proc_file;
 
-static struct proc_dir_entry *proc_entry;
-
-static ssize_t ldd_read(struct file *, char __user *, size_t, loff_t *){
-        printk("LDD: ldd_read");
-        return 0;
+//implemented read proc_op
+static ssize_t ldd_read(struct file *file, char __user *ubuf, size_t count, loff_t *offset){
+	printk("PROC_FS_LDD: ldd_read\n");
+	return 0;
 }
 
+//implemented write proc_op
+//must return size of input to prevent repeated/infinite attempts at writing
+static ssize_t ldd_write(struct file *file, const char __user *ubuf, size_t count, loff_t *offset){
+    printk("PROC_FS_LDD: ldd_write\n");
+    return count;
+}
+
+//linking implemented proc operations
 const struct proc_ops ldd_proc_ops = {
-        .proc_read = ldd_read
+	.proc_read = ldd_read,
+	.proc_write = ldd_write
 };
 
+//constructor function for kernel module
 static int ldd_init(void){
-        printk("LDD: ldd_init enter\n");
+	printk("PROC_FS_LDD: ldd_init enter\n");
 
-        proc_entry = proc_create("ldd", 0, NULL, &ldd_proc_ops);
+	//creating parent folder, if unsuccessful return error
+	proc_folder = proc_mkdir("testing", NULL);
 
-        if (proc_entry == NULL){
-                printk("LDD: ldd_init - proc_create failed");
-                return -1;
-        }
+	if (proc_folder == NULL){
+	    printk("PROC_FS_LDD: ldd_init - failed to create procfs folder.\n");
+		return -ENOMEM;
+	}
 
-        printk("LDD: ldd_init exit\n");
+	//creating proc file within parent folder, if unsuccessful remove parent folder and return error
+	proc_file = proc_create("ldd", 0666, proc_folder, &ldd_proc_ops);
 
-        return 0;
+	if (proc_file == NULL){
+		printk("PROC_FS_LDD: ldd_init - failed to create procfs file.\n");
+		proc_remove(proc_folder);
+		return -ENOMEM;
+	}
+
+	printk("PROC_FS_LDD: ldd_init exit\n");
+
+	return 0;
+
 }
+
+//destructor function for kernel module
+static void ldd_exit(void){
+	printk("PROC_FS_LDD: ldd_exit enter\n");
+
+	//removing folder entity recursively removes all other files within
+	proc_remove(proc_folder);
+
+	printk("PROC_FS_LDD: ldd_exit exit\n");
+}
+
+module_init(ldd_init);
+module_exit(ldd_exit);
