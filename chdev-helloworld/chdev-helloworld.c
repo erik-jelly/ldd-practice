34b3bc4e5ac16bed432a84b7113c5451cb4689bc
erik-jelly <ejelezarov11@gmail.com>
2026-08-21 13:13:31 -0400

    Added kernel logging levels to printk

--- chdev-helloworld/chdev-helloworld.c
+++ chdev-helloworld/chdev-helloworld.c
@@ -9,22 +9,35 @@ MODULE_DESCRIPTION("Basic module that registers a character device.");
 
 static int major_dev_no;
 
-static struct file_operations fops = {};
+static ssize_t read (struct file *f, char __user *ubuf, size_t ubuf_len, loff_t *offset) {
+    printk(KERN_INFO "CHR_DEV: read op called\n");
+    return 0;
+}
+
+static ssize_t write (struct file *f, const char __user *ubuf, size_t ubuf_len, loff_t *offset){
+    printk(KERN_INFO "CHR_DEV: write op called\n");
+    return ubuf_len;
+}
+
+static struct file_operations fops = {
+    .read = read,
+    .write = write
+};
 
 //constructor function for kernel module
 static int mymodule_init(void){
-	printk("CHR_DEV: mymodule_init enter\n");
+	printk(KERN_DEBUG "CHR_DEV: mymodule_init enter\n");
 
 	major_dev_no = register_chrdev(0, "mydev", &fops);
 
 	if (!major_dev_no) {
-	    printk("CHR_DEV: Device registration failed!\n");
+	    printk(KERN_ERROR "CHR_DEV: Device registration failed!\n");
 		return major_dev_no;
 	}
 
-	printk("CHR_DEV: Device registration sucess! Major device no. is %d\n", major_dev_no);
+	printk(KERN_INFO "CHR_DEV: Device registration sucess! Major device no. is %d\n", major_dev_no);
 
-	printk("CHR_DEV: mymodule_init exit\n");
+	printk(KERN_DEBUG "CHR_DEV: mymodule_init exit\n");
 
 	return 0;
 
@@ -32,11 +45,11 @@ static int mymodule_init(void){
 
 //destructor function for kernel module
 static void mymodule_exit(void){
-	printk("CHR_DEV: mymodule_exit enter\n");
+	printk(KERN_DEBUG "CHR_DEV: mymodule_exit enter\n");
 
 	unregister_chrdev(major_dev_no, "mydev");
 
-	printk("CHR_DEV: mymodule_exit exit\n");
+	printk(KERN_DEBUG "CHR_DEV: mymodule_exit exit\n");
 }
 
 module_init(mymodule_init);
